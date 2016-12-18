d94fb97614cb43a837eacf28935115613a6ac8e5
pascallimeux <pascal.limeux@orange.com>
2016-12-18 18:32:48 +0100

    add 2 uis for tests

---
+++ ui/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/andlabs/ui"
+	// go get github.com/andlabs/ui
+	//sudo apt-get install libgtk-3-dev
+)
+
+func main() {
+	err := ui.Main(func() {
+		name := ui.NewEntry()
+		button := ui.NewButton("Greet")
+		greeting := ui.NewLabel("")
+		box := ui.NewVerticalBox()
+		box.Append(ui.NewLabel("Enter your name:"), false)
+		box.Append(name, false)
+		box.Append(button, false)
+		box.Append(greeting, false)
+		window := ui.NewWindow("Hello", 200, 100, false)
+		window.SetChild(box)
+		button.OnClicked(func(*ui.Button) {
+			greeting.SetText("Hello, " + name.Text() + "!")
+		})
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		panic(err)
+	}
+}
